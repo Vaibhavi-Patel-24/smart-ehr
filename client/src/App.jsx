537012a722ec3fa8fbfb7134395b8ad9f1b12a55
Vaibhavi-Patel-24 <121277667+Vaibhavi-Patel-24@users.noreply.github.com>
2025-07-12 13:53:06 +0530

    final setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <>
-      hello
+    <div className="min-h-screen bg-base-200 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-500">143</h1>
+    </div>
     </>
   )
 }
