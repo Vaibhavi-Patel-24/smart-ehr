698cf0716e7f2e238cf1f737b2d05a654b50866f
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2025-07-12 14:04:39 +0530

    a

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,10 @@ function App() {
 
   return (
     <>
-      hello
+      <LoginCommon/>
+    <div className="min-h-screen bg-base-200 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-500">143</h1>
+    </div>
     </>
   )
 }
