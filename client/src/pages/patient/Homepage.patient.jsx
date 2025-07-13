1da9878d05755fd5c73e0937dd34123620f8e1fb
Vaibhavi-Patel-24 <121277667+Vaibhavi-Patel-24@users.noreply.github.com>
2025-07-13 22:41:18 +0530

    new

--- client/src/pages/patient/Homepage.patient.jsx
+++ client/src/pages/patient/Homepage.patient.jsx
@@ -7,14 +7,6 @@ import Navbar from '../../components/Navbar';
 const HomepagePatient = () => {
   return (
     <>
-    {/* <div className="absolute h-screen w-full flex justify-center items-center flex-col">
-        <p className='text-md font-semibold text-[rgb(255,143,154)]'>Don’t wait. If this is a health crisis, tap the SOS button now</p>
-        <img src={sos} alt = " sos " className='pt-10 w-25 h-auto'/>
-        <p className='text-md font-semibold text-[rgb(255,143,154)]'>EMERGENCY</p>
-        <div>
-            <EhrContainer data={data}/>
-        </div>
-    </div> */}
 
     <div className="min-h-screen bg-white">
       {/* Use Navbar as-is */}
@@ -22,7 +14,7 @@ const HomepagePatient = () => {
 
       {/* Push content down below fixed nav */}
       <div className="pt-20 px-4 flex flex-col items-center text-center space-y-6">
-        <p className="text-md font-semibold text-[rgb(255,143,154)] max-w-md">
+        <p className="text-md font-semibold text-[rgb(255,143,154)] max-w-md pt-6">
           Don’t wait. If this is a health crisis, tap the SOS button now
         </p>
 
