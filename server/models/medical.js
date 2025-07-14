c070511894ec0bdbe465a85a29a32bc444c1e027
Vaibhavi-Patel-24 <121277667+Vaibhavi-Patel-24@users.noreply.github.com>
2025-07-14 21:04:52 +0530

    medical model

---
+++ server/models/medical.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const medicalSchema = mongoose.Schema({
+    medicalId:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        require:true
+    },
+    brachName:{
+        type:String,
+        require:true
+    },
+    address:{
+        type:String,
+        require:true
+    },
+    contact:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    }
+},{timestamps: true});
+
+const Medical = mongoose.model('Medical',medicalSchema);
+export default Medical
