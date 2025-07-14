bcdb796cb97308dbc0d57f7474ce48410d6b47f4
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2025-07-14 18:45:27 +0530

    cors and dotenv configured

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
 import express from 'express'
+import router from './routes/route.js';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv'
+import cors from'cors'
 
-const PORT = 8000
+dotenv.config()
+
+const PORT = process.env.PORT || 8000
 const app = express();
+
+app.use(cors())
+
+//search kari n joi leje niche nu mn nai khbr
+app.use(express.json());
+app.use(bodyParser.json({ extended:true}))
+app.use(bodyParser.urlencoded({ extended:true}))
+
+// routes throgh this
+app.use('/',router)
 
 // see in package.json
 // to start for dev purposes use command: npm run dev
 // for production grade command is : npm start
-try{app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT} successfully`)
-})} catch{console.log(`server failed`)}
+try{
+    app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT} successfully`)})
+} 
+catch{console.log(`server failed to start`)}
