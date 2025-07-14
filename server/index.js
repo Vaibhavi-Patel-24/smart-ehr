25c2c62a61360089e823e17bd201ab43ea9bee00
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2025-07-14 18:32:17 +0530

    nodemon settled

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import express from 'express'
 const PORT = 8000
 const app = express();
 
+// see in package.json
+// to start for dev purposes use command: npm run dev
+// for production grade command is : npm start
 try{app.listen(PORT,()=>{
     console.log(`server running on port ${PORT} successfully`)
 })} catch{console.log(`server failed`)}
