da62866dde20a5272520ade64258f77487be589e
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2025-07-14 18:35:59 +0530

    routes file created

---
+++ server/routes/route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+// import all the controllers here from controller folder
+
+const router = express.Router();
+
+// place all routes(endpoints) here with methods like PUT, GET etc..
+
+export default router;
