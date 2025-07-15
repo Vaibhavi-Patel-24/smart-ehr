a589b47be76e801a04bff7f12a344d0f8e72a068
Vaibhavi-Patel-24 <121277667+Vaibhavi-Patel-24@users.noreply.github.com>
2025-07-15 21:08:45 +0530

    jwt

--- server/routes/route.js
+++ server/routes/route.js
@@ -4,27 +4,27 @@ import { loginPatient } from '../controllers/login.patient.controller.js';
 import { loginMedical } from '../controllers/login.medical.controller.js';
 import { loginAdmin } from '../controllers/login.admin.controller.js';
 import { addPatient, getAllPatients, getPatientById, updatePatientbyAdmin, updatePatientbyMedical, updatePatientSelf } from '../controllers/patient.controller.js';
-
+import auth from '../middleware/auth.js';
 // import all the controllers here from controller folder
 
 const router = express.Router();
-router.post('/medical/addmedical',createMedical);
-router.get('/medical/allmedical', getAllMedicals);
-router.get('/medical/:id', getMedicalById);
-router.delete('/medical/:id', deleteMedical);
-router.put('/medical/:id', updateMedical); // ← new route here
+router.post('/medical/addmedical',auth,createMedical);
+router.get('/medical/allmedical',auth, getAllMedicals);
+router.get('/medical/:id',auth, getMedicalById);
+router.delete('/medical/:id',auth, deleteMedical);
+router.put('/medical/:id',auth, updateMedical); // ← new route here
 
-router.post('/patient/addpatient', addPatient)
-router.get('/patient/allpatients',getAllPatients)
+router.post('/patient/addpatient',auth, addPatient)
+router.get('/patient/allpatients',auth,getAllPatients)
 
 router.post('/login/patient',loginPatient);
 router.post('/login/medical',loginMedical);
 router.post('/login/admin',loginAdmin)
 // place all routes(endpoints) here with methods like PUT, GET etc..
-router.get('/patient/:id',getPatientById)
-router.patch('/admin/patients/:id', updatePatientbyAdmin);
-router.patch('/medical/patients/:id', updatePatientbyMedical);
-router.patch('/patients/:id', updatePatientSelf);
+router.get('/patient/:id',auth,getPatientById)
+router.patch('/admin/patients/:id',auth, updatePatientbyAdmin);
+router.patch('/medical/patients/:id',auth, updatePatientbyMedical);
+router.patch('/patients/:id',auth, updatePatientSelf);
 
 
 
