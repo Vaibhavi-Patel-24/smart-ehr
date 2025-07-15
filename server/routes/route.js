f655f612bebc2aeed4a39a04c6a05df020ebdc4e
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2025-07-15 15:10:46 +0530

    .

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createMedical,getAllMedicals,getMedicalById,deleteMedical,updateMedical } from '../controllers/medical.controller.js';
-import { addPatient, getAllPatients } from '../controllers/patient.controller.js';
+import { addPatient, getAllPatients, getPatientById, updatePatientbyAdmin, updatePatientbyMedical, updatePatientSelf } from '../controllers/patient.controller.js';
 
 // import all the controllers here from controller folder
 
@@ -13,7 +13,11 @@ router.put('/medical/:id', updateMedical); // ← new route here
 
 router.post('/patient/addpatient', addPatient)
 router.get('/patient/allpatients',getAllPatients)
-// place all routes(endpoints) here with methods like PUT, GET etc..
+router.get('/patient/:id',getPatientById)
+router.patch('/admin/patients/:id', updatePatientbyAdmin);
+router.patch('/medical/patients/:id', updatePatientbyMedical);
+router.patch('/patients/:id', updatePatientSelf);
+
 
 
 export default router;
