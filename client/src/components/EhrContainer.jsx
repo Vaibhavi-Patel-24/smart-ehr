6582bae5eecb394155b7d9c6456cbdc268769820
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2025-07-16 11:59:04 +0530

    z index

--- client/src/components/EhrContainer.jsx
+++ client/src/components/EhrContainer.jsx
@@ -80,7 +80,7 @@ function EhrContainer({ data = {}, editMode }) {
   const age = calculateAge(dob);
 
   return (
-    <div className="border border-[#00B2FF] rounded-md p-4 w-full max-w-[600px] mx-auto bg-white text-[#444] font-inter text-sm">
+    <div className="border z-1000 border-[#00B2FF] rounded-md p-4 w-full max-w-[600px] mx-auto bg-white text-[#444] font-inter text-sm">
       <h2 className="text-xl font-semibold mb-2 text-[#00B2FF]">Electronic Health Record</h2>
 
       <input
